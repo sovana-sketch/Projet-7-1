1e77e532a697d12092cbaca031c489a69c151972
sovana-sketch <sovanahnpgg@gmail.com>
2026-03-26 20:58:11 +0100

    git commit -m "modalv0.2"

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/Admin.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/Admin.js
@@ -15,7 +15,7 @@ else {
     const logout_a = document.createElement('a');
     logout_a.href = '#';
     logout_a.textContent = 'logout';
-    logout.appendChild(logout_a);
+    logout_link.appendChild(logout_a);
     logout_a.addEventListener('click', () => {
         console.log('logout');
         window.localStorage.removeItem('token');
@@ -23,19 +23,56 @@ else {
     });
 }
 //filter disables
-const unfilters = document.querySelector('filters')
-if (token){
-   filters.style.display='none'
+const unfilters = document.querySelectorAll('[data-filter]');
+console.log(unfilters);
+if (token) {
+    unfilters.forEach(unfilter => {
+        unfilter.style.display = 'none';
+    });
+} else {
+    unfilters.forEach(unfilter => {
+        unfilter.style.display = 'block';
+    });
+}
+
+//modal btn
+
+const edit_btn = document.getElementById('edit-btn');
+if (token === null) {
+    edit_btn.style.display = 'none';
 }
 else {
-    filters.style.display='block'
+    edit_btn.style.display = 'block';
 }
-//modal
+const valid_btn = document.getElementById('valid_btn');
+edit_btn.addEventListener('click', () => {
+    mode_edition.classList.add('show');
+});
+const btn_back = document.getElementById('btn-back');
+const mode_edition = document.getElementById('mode_edition');
+const mode_edition2 = document.getElementById('mode_edition2');
 
-const modalbtns = document.getElementById('edit-btn');
-if (token) {
-    modal.style.display = 'block';
-  
-}
-else {    modal.style.display = 'none';
-}
+valid_btn.addEventListener('click', () => {
+    mode_edition2.classList.add('show');
+});
+btn_back.addEventListener('click', () => {
+    mode_edition.classList.add('show');
+    mode_edition2.classList.remove('show');
+});
+
+const btn_close = document.querySelectorAll('.btn_close');
+btn_close.forEach(btn => {
+    btn.addEventListener('click', () => {
+        mode_edition.classList.remove('show');
+        mode_edition2.classList.remove('show');
+    });
+});
+
+[mode_edition, mode_edition2].forEach(modal => {
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            mode_edition.classList.remove('show');
+            mode_edition2.classList.remove('show');
+        }
+    });
+});
